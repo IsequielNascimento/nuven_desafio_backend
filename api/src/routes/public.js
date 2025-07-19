d07eb0d8481818fdd98a9324274c8b6c244f68aa
IsequielNascimento <isequielnascimento@gmail.com>
2025-07-19 16:30:36 -0300

    feat(auth/register): implementação do registro do usuário

--- api/src/routes/public.js
+++ api/src/routes/public.js
@@ -1,11 +1,29 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
 
+const prisma = new PrismaClient()
 const router = express.Router()
 
-router.post('/auth/register', (req, res) => {
-    const user = req.body
+router.post('/auth/register', async (req, res) => {
+    try {
+        const user = req.body
 
-    res.status(201).json(user)
+        const salt = await bcrypt.genSalt(5)
+        const hashPassword = await bcrypt.hash(user.password, salt)
+
+        // remover 'const userDB =' no final dos testes
+        const userDB = await prisma.user.create({
+            data: {
+                name: user.name,
+                email: user.email,
+                password: hashPassword,
+            }
+        })
+        res.status(201).json(userDB)//alter 'userDB' parar alguma mensagem genérica 201
+    }catch (err) {
+        res.status(500).json({ message: "Erro ao conectar ao servidor, tente novamente." })
+    }
 })
 
 export default router
