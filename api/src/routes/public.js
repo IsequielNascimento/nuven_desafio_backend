7721c24a717ec5f55c695e148ceafe989d37997b
IsequielNascimento <isequielnascimento@gmail.com>
2025-07-19 17:00:22 -0300

    feat(auth/login): implementando a lógica de login e token jwt

--- api/src/routes/public.js
+++ api/src/routes/public.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
 
 const prisma = new PrismaClient()
 const router = express.Router()
+
+const JWT_SECRET = process.env.JWT_SECRET
+// REGISTER
 
 router.post('/auth/register', async (req, res) => {
     try {
@@ -21,9 +26,45 @@ router.post('/auth/register', async (req, res) => {
             }
         })
         res.status(201).json(userDB)//alter 'userDB' parar alguma mensagem genérica 201
-    }catch (err) {
+    } catch (error) {
         res.status(500).json({ message: "Erro ao conectar ao servidor, tente novamente." })
     }
+})
+
+
+// LOGIN
+
+router.post('/auth/login', async (req, res) => {
+
+    try {
+
+        const userInfo = req.body
+
+        const user = await prisma.user.findUnique({
+            where: { email: userInfo.email }
+        })
+
+        if (!user) {
+            return res.status(404).json({ message: "Usuário não encontrado" })
+        }
+
+        const isMatch = await bcrypt.compare(userInfo.password, user.password)
+
+        if(!isMatch){
+            return res.status(400).json({message: 'Senha inválida'})
+        }
+
+        //Gerar Token JWT
+
+        const token = jwt.sign({id: user.id}, JWT_SECRET, {expiresIn: '8h'})
+
+        res.status(200).json(token)
+
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao conectar ao servidor, tente novamente." })
+    }
+
+
 })
 
 export default router
